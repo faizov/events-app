d78b543706f13e2fa060b598f222315eec656bdc
Yuriy <43875549+faizov@users.noreply.github.com>
2022-10-02 11:20:24 +0300

    Fix render

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-
 import App from "./App";
 
-ReactDOM.render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+const rootElement = document.getElementById("root");
+
+// 👇️ use type assertion
+const root = createRoot(rootElement as Element);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
